3df93f959ff8267cfc262199499356ab418577dd
ejembi22 <ejembi_emma@icloud.com>
2024-08-30 07:09:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+if (4>3){
+    result=true;
+} else{
+    result=false;
+}
+console.log(result);
+
+
+if (4 >= 3){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if (4<3){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4<=3){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 == 4){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 === 4){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 != 4){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 !== 4){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 != '4'){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if(4 == '4'){
+    result=true;
+}else{
+    result=false;
+}
+console.log(result);
+
+if (4 === '4'){
+    result=true;
+} else{
+    result=false;
+}
+console.log(result);
+
+
+// Part B 
+
+let myDate =new Date()
+console.log(myDate.getFullYear())
+
+
+
+// let myNewMonth =new Date ()
+// let myMonth=myNewMonth.getMonth();
+// console.log(myMonth)
+
+let myYear = myDate.getFullYear()
+console.log("What is the year today " + myYear )
+
+let myMonth = myDate.getMonth()
+console.log(myMonth)
+
+let myTodayDate = new Date()
+console.log("What is today date " + myTodayDate )
+
+let todayDate =myDate.getDay()
+console.log(todayDate)
+
+let hours = myDate.getHours ()
+console.log(hours)
+
+let minutes = myDate.getMinutes()
+    console.log(minutes)
+
+ 
